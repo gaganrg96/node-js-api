aadece489b3925b7d5059402a4cc9992e0248e47
gaganrg96 <133869996+gaganrg96@users.noreply.github.com>
2025-04-15 13:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome back Gagan to the Node.js Application API');
+    res.send('Welcome back Gagan to the Node.js Application API 2');
 });
 
 app.listen(3000, () => {
