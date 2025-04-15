80a123d64d4b9466321c5d21a2dd7c62cdc75c14
gaganrg96 <133869996+gaganrg96@users.noreply.github.com>
2025-04-15 12:57:28 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Node.js API! Welcome Gagan to Node.js API');
+    res.send('Welcome Gagan to Node.js API');
 });
 
 app.listen(3000, () => {
